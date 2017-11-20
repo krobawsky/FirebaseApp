422bb20ac83498dc856578feff6e0f75cff152db
Krobawsky <ricardo.berrospi@tecsup.edu.pe>
2017-11-20 12:53:13 -0500

    krobawsky

---
+++ app/src/main/java/tecsup/rberrospi/lab05/firebaseapp/MainActivity.java
@@ -0,0 +1,64 @@
+package tecsup.rberrospi.lab05.firebaseapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+    private DatabaseReference mDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        Bundle bundle = new Bundle();
+        bundle.putString("fullname", "Ricardo Berrospi");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.APP_OPEN, bundle);
+
+        mFirebaseAnalytics.setUserProperty("username", "rberrospi");
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        Log.d(TAG, "user: " + user);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                callLogout(null);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void callLogout(View view){
+        Log.d(TAG, "Sign out user");
+        FirebaseAuth.getInstance().signOut();
+        finish();
+    }
+}
---
+++ app/src/main/java/tecsup/rberrospi/lab05/firebaseapp/MyFirebaseInstanceIDService.java
@@ -0,0 +1,23 @@
+package tecsup.rberrospi.lab05.firebaseapp;
+
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = MyFirebaseInstanceIDService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        FirebaseMessaging.getInstance().subscribeToTopic("ALL");
+    }
+
+}
